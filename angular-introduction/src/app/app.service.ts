42c57ff4ec332d5b08be6d7ed6a4a2e8180d3794
G.P.Kampouris <georgeokamps@gmail.com>
2023-10-26 17:12:17 +0300

    delete users

--- angular-introduction/src/app/app.service.ts
+++ angular-introduction/src/app/app.service.ts
@@ -1,23 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable, inject } from '@angular/core';
 import { Person } from './interfaces/person';
+//import { DeleteUserComponent } from './crud-demo/delete-user/delete-user.component';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AppService {
- constructor(private http: HttpClient = inject(HttpClient)) { }
+  constructor(private http: HttpClient = inject(HttpClient)) {}
 
- getAllUsers() {
-   return this.http.get<Person[]>('http://localhost:3000/users')
- }
+  getAllUsers() {
+    return this.http.get<Person[]>('http://localhost:3000/users');
+  }
 
- getUserById(id: number) {
-  return this.http.get<Person>(`http://localhost:3000/users/${id}`)
- }
+  getUserById(id: number) {
+    return this.http.get<Person>(`http://localhost:3000/users/${id}`);
+  }
 
- addUser(user : Person){
-  return this.http.post<Person>('http://localhost:3000/users', user)
- }
+  addUser(user: Person) {
+    return this.http.post<Person>('http://localhost:3000/users', user);
+  }
 
+  deleteUser(id: number) {
+    return this.http.delete<Person>(`http://localhost:3000/users/${id}`);
+  }
 }
