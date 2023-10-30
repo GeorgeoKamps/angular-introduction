2899118a87af51e28d46aefff3213070e92d8130
G.P.Kampouris <georgeokamps@gmail.com>
2023-10-30 20:23:59 +0200

    event bing porting

--- angular-introduction/src/app/app.component.ts
+++ angular-introduction/src/app/app.component.ts
@@ -1,81 +1,16 @@
-import { Component, Inject, OnInit } from '@angular/core';
+import { Component,  } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Person } from './interfaces/person';
 
-import { PersonComponent } from './person/person.component';
-import { PersonAltComponent } from './person-alt/person-alt.component';
-import { EventBindComponent } from './event-bind/event-bind.component';
-import { OutputDemoComponent } from './output-demo/output-demo.component';
-import { PersonCardComponent } from './person-card/person-card.component';
-import { TemplateDrivenFormComponent } from './template-driven-form/template-driven-form.component';
-import { ReactiveFormComponent } from './reactive-form/reactive-form.component';
-import { AppService } from './app.service';
-import { CrudDemoComponent } from './crude-demo/crud-demo/crud-demo.component';
 import { ApplicationLayoutComponent } from './application-layout/application-layout.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [
-    CommonModule,
-    PersonComponent,
-    PersonAltComponent,
-    EventBindComponent,
-    OutputDemoComponent,
-    PersonCardComponent,
-    TemplateDrivenFormComponent,
-    ReactiveFormComponent,
-    CrudDemoComponent,
-    ApplicationLayoutComponent
-
+    CommonModule,ApplicationLayoutComponent
 ],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  name: string = "Georgeo";
-  lastname = "Kampouris";
-
-    person: Person ={
-     givenName: "Georgeo",
-     surName: "Kampouris",
-     age: 25,
-     email: "georgeokamps@gmail.com",
-    address: "skg"
-    };
-
-    person2: Person ={
-        givenName: "akis",
-        surName: "stapasidis",
-        age: 25,
-        email: "stapass@gmail.com",
-      address: "skg"
-     };
-
-   users: Person[] = [];
-
-    sendUser: Person | undefined;
-
-    constructor(private appService: AppService = Inject(AppService)){}
-
-    ngOnInit(): void {
-      this.appService.getAllUsers().subscribe((users) => {
-        this.users = users
-        console.log(this.users);
-      });
-    }
-
-    onDeleteUser(i:number) {
-      this.users.splice(i,1)
-    }
-
-    onSendUser(user:Person){
-      console.log(user);
-      this.sendUser = user;
-    }
-
-    onNewPerson(person: Person){
-      this.users.push(person);
-    }
-};
+export class AppComponent  {}
 
--- angular-introduction/src/app/event-bind/event-bind.component.ts
+++ angular-introduction/src/app/event-bind/event-bind.component.ts
@@ -1,27 +1,37 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-event-bind',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatCardModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+  ],
   templateUrl: './event-bind.component.html',
-  styleUrls: ['./event-bind.component.css']
+  styleUrls: ['./event-bind.component.css'],
 })
 export class EventBindComponent {
   times: number = 0;
-  userInput: string = "";
+  userInput: string = 'Type Something';
 
-  increaseCounter(){
+  increaseCounter() {
     this.times += 1;
   }
 
-  reserCounter(){
+  resetCounter() {
     this.times = 0;
   }
 
   onUserInput(event: Event) {
     const target = event.target as HTMLInputElement;
-    this.userInput = target.value; 
+    this.userInput = target.value;
   }
 }
